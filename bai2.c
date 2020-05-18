1620faffc2140bf8f3900e036ee08ddea9ba50ef
cedrus172 <64965926+cedrus172@users.noreply.github.com>
2020-05-18 11:23:18 +0700

    Create bai2.c

---
+++ bai2.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    for (int i=1;i <= 4;i++)
+    {
+    for (int ia=1;ia <= i;ia++)
+        {
+            printf("%d", ia);
+        }
+        printf("\n");
+    }
+}
+
